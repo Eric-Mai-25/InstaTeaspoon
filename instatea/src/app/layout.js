1600e72688d9dd0258d454af0f606c98a6d209b8
Eric-Mai-25 <ericmai2@gmail.com>
2024-06-29 14:20:17 -0700

    layout

--- instatea/src/app/layout.js
+++ instatea/src/app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
       
     <html lang="en">
       <body className={inter.className}>
-      <Header/>        <Header/>        <Header/>
+        <Header/>
         {children}</body>
     </html>
     </SessionWrapper>
