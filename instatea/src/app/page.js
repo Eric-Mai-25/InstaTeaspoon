0e928a47b93c5961816fb4510bf96edb2fa0dfa9
Eric-Mai-25 <ericmai2@gmail.com>
2024-06-29 14:21:19 -0700

    page start

--- instatea/src/app/page.js
+++ instatea/src/app/page.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <h1 >page</h1>
-    <p>Hello there </p>
+    <div>
+      <h1 >page</h1>
+      <p>Hello there </p>
+
+    </div>
   )
 }
